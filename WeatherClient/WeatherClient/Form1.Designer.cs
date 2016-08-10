572fe1c858316d434997ddd06b4b1613fb752646
Ben Farmer <ben_farmer3@hotmail.com>
2016-08-10 22:07:45 +0100

    Add files via upload

---
+++ WeatherClient/WeatherClient/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace WeatherClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnArtistNumbers = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(117, 71);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Get Wind Speed";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(117, 132);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Send Wind Speed";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnArtistNumbers
+            // 
+            this.btnArtistNumbers.Location = new System.Drawing.Point(117, 176);
+            this.btnArtistNumbers.Name = "btnArtistNumbers";
+            this.btnArtistNumbers.Size = new System.Drawing.Size(115, 23);
+            this.btnArtistNumbers.TabIndex = 2;
+            this.btnArtistNumbers.Text = "Get number of artists";
+            this.btnArtistNumbers.UseVisualStyleBackColor = true;
+            this.btnArtistNumbers.Click += new System.EventHandler(this.btnArtistNumbers_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.btnArtistNumbers);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnArtistNumbers;
+    }
+}
+
---
+++ WeatherClient/WeatherClient/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WeatherClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            WeatherService.WeatherSvcClient weatherServiceClient = new WeatherService.WeatherSvcClient();
+            //var temp = weatherServiceClient.GetCurrentTemp();
+            var windSpeed = weatherServiceClient.GetWindSpeed();
+            MessageBox.Show(windSpeed.ToString());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            WeatherService.WeatherSvcClient weatherServiceClient = new WeatherService.WeatherSvcClient();
+            weatherServiceClient.SendWindSpeed(34.2F);
+        }
+
+        private void btnArtistNumbers_Click(object sender, EventArgs e)
+        {
+            WeatherService.WeatherSvcClient weatherServiceClient = new WeatherService.WeatherSvcClient();
+            int numberOfArtists = weatherServiceClient.GetNumberOfArtists();
+            MessageBox.Show(numberOfArtists.ToString());
+        }
+    }
+}
